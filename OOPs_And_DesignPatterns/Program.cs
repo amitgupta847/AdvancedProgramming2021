d669902e609e71e22e93ba0c075079c50ae51b99
amitgupta847 <amit847.swastic@gmail.com>
2021-03-09 19:57:50 -0500

    Double Dispatcher added

--- OOPs_And_DesignPatterns/Program.cs
+++ OOPs_And_DesignPatterns/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OOPs_And_DesignPatterns.cBehavioral.VisitorPattern.AirCraftExample;
 using OOPs_And_DesignPatterns.cBehavioral.VisitorPattern.AirCraftExample2;
+using OOPs_And_DesignPatterns.cBehavioral.VisitorPattern.AirCraftExample3;
 using System;
 
 namespace OOPs_And_DesignPatterns
@@ -13,7 +14,8 @@ namespace OOPs_And_DesignPatterns
 
       //Behavioral
       //AirCraft_V1_AsVisitorPattern.StartMethod();
-      AirCraft_V2_AsVisitorPattern.StartMethod();
+      //AirCraft_V2_AsVisitorPattern.StartMethod();
+      AirCraft_V3_DoubleDispatcher.StartMethod();
 
       Console.WriteLine("Design pattern testing done!");
       Console.ReadLine();
---
+++ OOPs_And_DesignPatterns/cBehavioral/VisitorPattern/AirCraftExample/AirCraft_V3_DoubleDispatcher.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPs_And_DesignPatterns.cBehavioral.VisitorPattern.AirCraftExample3
+{
+
+  //In this class i am trying to create abstract class for aircraft and then a separate interface for IVisitable
+
+  /// <summary>
+  /// Entry Point
+  /// </summary>
+  public class AirCraft_V3_DoubleDispatcher
+  {
+    public static void StartMethod()
+    {
+
+      F16 f16 = new F16();
+      F16 betterF16 = new BetterF16();
+
+      Missile missile = new Missile();
+      Missile betterMissile = new BetterMissile();
+
+      Console.WriteLine("Expected output");
+      f16.fireMissile(missile);
+      betterF16.fireMissile(missile);
+      Console.WriteLine();
+
+      Console.WriteLine("Failed double dispatch attempt");
+      f16.fireMissile(betterMissile);
+      betterF16.fireMissile(betterMissile);
+      Console.WriteLine();
+
+      Console.WriteLine("Expected output");
+      BetterMissile reallyBetterMissile = new BetterMissile();
+      f16.fireMissile(reallyBetterMissile);
+      betterF16.fireMissile(reallyBetterMissile);
+      Console.WriteLine();
+
+      return;
+    }
+  }
+
+  public class F16
+  {
+    public virtual String WhoAmI()
+    {
+      return "F16";
+    }
+
+    public void fireMissile(Missile missile)
+    {
+      Console.WriteLine(WhoAmI() + " fired ordinary missile: " + missile.Explode());
+    }
+
+    public void fireMissile(BetterMissile missile)
+    {
+      Console.WriteLine(WhoAmI() + " fired better missile: " + missile.Explode());
+    }
+
+  }
+
+  public class BetterF16 : F16
+  {
+    public override String WhoAmI()
+    {
+      return "Better F16";
+    }
+  }
+
+  public class Missile
+  {
+    public virtual string Explode()
+    {
+      return "baaaam";
+    }
+  }
+
+  public class BetterMissile : Missile
+  {
+    public override string Explode()
+    {
+      return "very very big baaaam";
+    }
+  }
+
+
+}
